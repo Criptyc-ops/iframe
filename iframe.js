2e88b45a3da629fa743d9b545241607df9f846fc
Poke13617 <cte-mason.curtis@sllboces.org>
2024-11-07 16:52:35 +0000

    js start...

--- iframe.js
+++ iframe.js
@@ -1,1 +1,12 @@
-.
+// javascript
+mw.util.addCSS(
+    'iframe.sbframe {/n' +
+    '/twidth: 180px;/n' +
+    '/toverflow: hidden;/n' +
+    '/tborder: none;/n' +
+    '}'
+);
+$("span.sbframe").each(function() {
+    var widget,
+        dataWidget = $(this).
+})
