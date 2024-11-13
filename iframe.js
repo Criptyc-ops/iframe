e0fefcde13f9ba7522a492b497fa6d96e8ef921d
Poke13617 <cte-mason.curtis@sllboces.org>
2024-11-13 18:11:29 +0000

    js start...

--- iframe.js
+++ iframe.js
@@ -1,12 +1,27 @@
-// javascript
+//javascript
 mw.util.addCSS(
-    'iframe.sbframe {/n' +
-    '/twidth: 180px;/n' +
-    '/toverflow: hidden;/n' +
-    '/tborder: none;/n' +
+    'iframe.sbframe {\n' +
+       '\twidth: 180px;\n' +
+       '\theight: 190px;\n' +
+       '\toverflow: hidden;\n' +
+       '\tborder: none;\n' +
     '}'
-);
-$("span.sbframe").each(function() {
+)
+
+$("span.sbframe").each(function() {}
+
     var widget,
-        dataWidget = $(this).
-})
+    
+    dataWidget = $(this).attr("data-widget");
+    
+    try {
+    
+    widget = dataWidget.match(/[a-z0-9]+/i)[0];
+    
+    $(this).replaceWith('<iframe class="sbframe" src="widget.socialblade.com/widget.php?v=2&u=' + widget + '" scrolling="no" frameBorder="0" />');
+    
+    } catch(err) {
+    
+    console.log("Invalid '.sbframe' widget id: " + dataWidget);
+    
+    }
